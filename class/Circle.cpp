2c67fbfb7fc06bcd60f9a78cda1a3055397da0ad
Maciej <m567ende@gmail.com>
2025-12-05 05:54:15 +0100

    Addded changing object parameters via public fields

--- class/Circle.cpp
+++ class/Circle.cpp
@@ -12,13 +12,23 @@ int main ()
     //Syntax:  (radius,x,y)
     Circle ball(5.1,1,1);
     std::cout << "Object area: " << ball.area() << "\n" ;
-    std::cout << "Object center coordinates: \n";
+    std::cout << "Object center coordinates: ";
     ball.center.display();
 
-    //Modyfying object parameters via setters:
+    //Modifying object parameters via setters:
     ball.set_radius(10);
     ball.set_origin(3.1,4.2);
-    std::cout << "\nObject parameters have been modified\n\n";
+    std::cout << "\nObject parameters have been modified via setters\n\n";
+
+    //Print object parameters again
+    std::cout << "Object area: " << ball.area() << "\n" ;
+    std::cout << "Object center coordinates: ";
+    ball.center.display();
+
+    //Modifying via public variables:
+    ball.center.x = 0;
+    ball.center.y = 0;
+    std::cout << "\nObject parameters have been modified via public fields\n\n";
 
     //Print object parameters again
     std::cout << "Object area: " << ball.area() << "\n" ;
