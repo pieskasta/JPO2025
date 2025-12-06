b2106919afb9930bfe6b1ddcc81f870280bd623e
pieskasta <121586941+pieskasta@users.noreply.github.com>
2025-12-06 11:00:56 +0100

    Add files via upload

---
+++ inheritance/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Animal {
+public:
+    virtual void makeSound() {
+    std::cout << "The sound of animal"<< std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void makeSound() override {
+        std::cout << "Woof!" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    void makeSound() override {
+        std::cout << "Meow";
+    }
+};
+
+Animal chooseAnimal(int number) {
+    if (number%2 == 0) {
+        return 0; /* TODO return dynamically created Dog object */
+    } else {
+        return 0; /* TODO return dynamically created Cat object */
+    }
+}
+
+int main() {
+    std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+
+    Animal *animal = chooseAnimal(x);
+    animal->makeSound();
+
+    /* TODO properly free dynamically allocated object */
+    delete animal;
+
+    return 0;
+}
